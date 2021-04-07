70810be7eab4fdfa11bd10daffc23562e18f1e84
Orlando Álvarez <wiki97ap@gmail.com>
2021-04-07 11:30:05 -0500

    #1 Fixes
    Added Component and Screens Files

---
+++ reservacionVuelo/Screens/SignUp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export class SignUp extends Component {
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}

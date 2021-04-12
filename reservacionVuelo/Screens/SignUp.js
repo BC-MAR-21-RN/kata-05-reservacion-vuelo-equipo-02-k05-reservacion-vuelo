ea419087a60a419d70ed8166c2036d523a77d8c4
Orlando Álvarez <wiki97ap@gmail.com>
2021-04-12 10:52:24 -0500

    #3 CR changes

--- reservacionVuelo/Screens/SignUp.js
+++ reservacionVuelo/Screens/SignUp.js
@@ -1,111 +1,122 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
 export class SignUp extends Component {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <View style={styles.mainContainer} >
-                <Text style={styles.titleStyle}>Sign Up</Text>
-                {/**TextInputs */}
-                <View style={styles.textInputSpacing}>
-                    <Text style={styles.textInputTitle}>First Name</Text>
-                    <TextInput style={styles.textInputStyle}></TextInput>
-                </View>
-                <View style={styles.textInputSpacing}>
-                    <Text style={styles.textInputTitle}>E-mail *</Text>
-                    <TextInput style={styles.textInputStyle}></TextInput>
-                </View>
-                <View style={styles.textInputSpacing}>
-                    <Text style={styles.textInputTitle}>Password *</Text>
-                    <TextInput style={styles.textInputStyle} secureTextEntry={true}></TextInput>
-                </View>
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.titleStyle}>Sign Up</Text>
+        {/**TextInputs */}
+        <View style={styles.textInputSpacing}>
+          <Text style={styles.textInputTitle}>First Name</Text>
+          <TextInput style={styles.textInputStyle} />
+        </View>
+        <View style={styles.textInputSpacing}>
+          <Text style={styles.textInputTitle}>E-mail *</Text>
+          <TextInput style={styles.textInputStyle} />
+        </View>
+        <View style={styles.textInputSpacing}>
+          <Text style={styles.textInputTitle}>Password *</Text>
+          <TextInput style={styles.textInputStyle} secureTextEntry={true} />
+        </View>
 
-                {/**CheckBoxes */}
-                <View style={styles.checkBoxAlignment}>
-                    <CheckBox tintColors={{ true: '#5974F5' }} />
-                    <Text style={styles.bottomTextStyle}>I agree to the <Text style={styles.hyperlinkText}>Terms</Text> and <Text style={styles.hyperlinkText}>Privacy Policy</Text></Text>
-                </View>
-                <View style={styles.checkBoxAlignment}>
-                    <CheckBox tintColors={{ true: '#5974F5' }} />
-                    <Text style={styles.bottomTextStyle}>Subscribe for select product update</Text>
-                </View>
+        {/**CheckBoxes */}
+        <View style={styles.checkBoxAlignment}>
+          <CheckBox tintColors={{true: '#5974F5'}} />
+          <Text style={styles.bottomTextStyle}>
+            I agree to the <Text style={styles.hyperlinkText}>Terms</Text> and{' '}
+            <Text style={styles.hyperlinkText}>Privacy Policy</Text>
+          </Text>
+        </View>
+        <View style={styles.checkBoxAlignment}>
+          <CheckBox tintColors={{true: '#5974F5'}} />
+          <Text style={styles.bottomTextStyle}>
+            Subscribe for select product update
+          </Text>
+        </View>
 
-                {/* Buttons */}
-                <TouchableHighlight style={styles.buttonStyle}>
-                    <Text style={styles.buttonTextStyle}>Sign Up</Text>
-                </TouchableHighlight>
-                <Text style={{ textAlign: 'center', color: '#A3ACB9', fontSize: 15, paddingVertical: 15 }}>or</Text>
-                <TouchableHighlight style={styles.buttonStyle}>
-                    <Icon name="google" color={'white'} size={15}>
-                            <Text style={styles.buttonTextStyle}> Login with Google</Text>
-                    </Icon>
-                </TouchableHighlight>
-                <Text style={styles.bottomTextStyle}>Already have an acoount? <Text style={styles.hyperlinkText}>Log in</Text></Text>
-
-            </View>
-        )
-    }
+        {/* Buttons */}
+        <TouchableHighlight style={styles.buttonStyle}>
+          <Text style={styles.buttonTextStyle}>Sign Up</Text>
+        </TouchableHighlight>
+        <Text style={styles.bottomTextStyle}>or</Text>
+        <TouchableHighlight style={styles.buttonStyle}>
+          <Icon name="sc-google-plus" color={'white'} size={15}>
+            <Text style={styles.buttonTextStyle}> Login with Google</Text>
+          </Icon>
+        </TouchableHighlight>
+        <Text style={styles.bottomTextStyle}>
+          Already have an acoount?{' '}
+          <Text style={styles.hyperlinkText}>Log in</Text>
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        paddingHorizontal: 30,
-        flex: 1,
-    },
-    titleStyle: {
-        fontSize: 40,
-        fontWeight: '700',
-        color: '#5974F5'
-    },
-    textStyle: {
-        color: '#868686',
-    },
-    bottomTextStyle: {
-        textAlign: 'center',
-        color: '#A3ACB9',
-        fontSize: 15,
-        paddingVertical: 10
-    },
-    textInputTitle: {
-        fontSize: 15,
-        paddingBottom: 5
-    },
-    textInputStyle: {
-        borderWidth: 1,
-        borderBottomColor: 'black',
-        width: 350,
-        height: 50,
-    },
-    textInputSpacing: {
-        paddingVertical: 10
-    },
-    checkBoxAlignment: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        right: 5
-    },
-    buttonStyle: {
-        backgroundColor: '#5974F5',
-        borderRadius: 5,
-        height: 50,
-        alignItems:'center',
-        paddingTop:15
-    },
-    buttonTextStyle: {
-        
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 15
-    },
-    hyperlinkText: {
-        color: '#5974F5',
-        textDecorationLine: 'underline'
-    }
-
-})
+  mainContainer: {
+    paddingHorizontal: 30,
+    flex: 1,
+  },
+  titleStyle: {
+    fontSize: 40,
+    fontWeight: '700',
+    color: '#5974F5',
+  },
+  textStyle: {
+    color: '#868686',
+  },
+  bottomTextStyle: {
+    textAlign: 'center',
+    color: '#A3ACB9',
+    fontSize: 15,
+    paddingVertical: 10,
+  },
+  textInputTitle: {
+    fontSize: 15,
+    paddingBottom: 5,
+  },
+  textInputStyle: {
+    borderWidth: 1,
+    borderBottomColor: 'black',
+    width: 350,
+    height: 50,
+  },
+  textInputSpacing: {
+    paddingVertical: 10,
+  },
+  checkBoxAlignment: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    right: 5,
+  },
+  buttonStyle: {
+    backgroundColor: '#5974F5',
+    borderRadius: 5,
+    height: 50,
+    alignItems: 'center',
+    paddingTop: 15,
+  },
+  buttonTextStyle: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 15,
+  },
+  hyperlinkText: {
+    color: '#5974F5',
+    textDecorationLine: 'underline',
+  },
+});
